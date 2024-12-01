450b86e918f4ccf1eb961875a92b0baf48722b8e
rpycgo <skyd0608@gmail.com>
2024-12-01 12:04:05 +0000

    test

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -7,5 +7,4 @@ echo "> 🔵 Stop & Remove docker services."
 sudo docker-compose down
 
 echo "> 🟢 Run new docker services."
-sudo docker-compose up -d --build
-
+sudo docker-compose up -d --build >> /var/log/deploy.log 2>&1

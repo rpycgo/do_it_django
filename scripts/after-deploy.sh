b65dd86648b1b47b096408db529470dccbb7ed60
rpycgo <skyd0608@gmail.com>
2024-12-01 10:38:05 +0000

    test

---
+++ scripts/after-deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+REPOSITORY=/home/ubuntu/
+
+cd $REPOSITORY/do_it_django
+
+echo "> 🔵 Stop & Remove docker services."
+docker compose down
+
+echo "> 🟢 Run new docker services."
+docker compose up --build -d
+
